74ad876751d22c7470ea1c61a274817926523608
Traian Stanciu (from Travis CI) <deploy@travis-ci.org>
2019-09-03 17:48:45 +0000

    Deploy traian-s/mars-rovers to github.com/traian-s/mars-rovers.git:gh-pages

---
+++ precache-manifest.476786e0a1a9dfda27ec8073b033060a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b9536744e207dad6ef14ea455fd04e",
+    "url": "/mars-rovers/index.html"
+  },
+  {
+    "revision": "f9dc0fd8ba86201ca6a8",
+    "url": "/mars-rovers/static/css/main.73f10bf6.chunk.css"
+  },
+  {
+    "revision": "d3ac1f4d6e38100fbc18",
+    "url": "/mars-rovers/static/js/2.7f05aa0e.chunk.js"
+  },
+  {
+    "revision": "f9dc0fd8ba86201ca6a8",
+    "url": "/mars-rovers/static/js/main.d727aa43.chunk.js"
+  },
+  {
+    "revision": "2761ef1ca2e7c2bde6b6",
+    "url": "/mars-rovers/static/js/runtime~main.cc9a4fc9.js"
+  }
+]);
